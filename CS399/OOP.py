9d12b6864deadab0a676c0c65f69ec72aad80c2a
nivlac3213 <80916176+Nivlac3213@users.noreply.github.com>
2023-02-09 20:50:59 -0700

    created the Sensor class in OOP and some subclasses

--- CS399/OOP.py
+++ CS399/OOP.py
@@ -5,41 +5,57 @@ Author: Calvin Henggeler
 """
 
 # ***** Class vs Objects *****
-# CLASS: collection of instance variables. A blueprint or template for objects
+# CLASS: collection of instance variables. A blueprint or template for objects.
 # OBJECTS: an Instance of a class
 
-# Python objects have Contructors __new__ and initilizers __init__
+# Python objects have constructors __new__ and inits __init__
 
 # Method/ operator overloading does not work in Python (ex .add() )
-# how ever a child class can override a mathod of a parent class
+# however a child class can override a method of a parent class
+
+import random
 
 
-class ExampleClass:
-    """ An example class for generic objects"""
+class Sensor:
+    """ Sensor Class for generic Sensor """
+    frequency = 10  # (Hz) can call the sensor every 10 time per second
 
-    def __int__(self):
+    def __int__(self, location: str):
         """
-        __init__ is an initilizer to initilze an object
-        :return: None, Python 3.11 -> Self
+        Initialize the new ob ject of type Sensor
+        :param location: str -> where the sensor is located
+        :return: none
         """
-        pass
+        self.location = location
+        self.req_counter = 0
 
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-    def get_name(self) -> str:
-        return self.name
-
-    def print_name(self) -> None:
-        print(self.name)
+    def __str__(self):
+        return self.__class__.__name__
 
 
-class ChildClass(ExampleClass):
-    """Child Class inheriting from parent ExampleClass"""
+class HumiditySensor(Sensor):
+    """
+    HumiditySensor class models a humidity sensor
+    Inherits from Sensor Class
+    """
+    frequency = 50  # (Hz)
 
-    def __init__(self):
-        pass
+    def read_humidity(self) -> str:
+        return f"Humidity in the {self.location} is {random.randint(0,90)}"
 
 
-if __name__ == '__main__':
-    exampleObj = ExampleClass('Big Cheese')
+class TempSensor(Sensor):
+
+    def __init__(self, location: str, unit: str):
+        self.unit = unit
+        super().__init__(location)
+
+    def read_temp(self) -> str:
+        self.req_counter += 1
+        if self.req_counter % self.frequency == 0:
+            return f"Humidity in the {self.location} is {random.randint(0,102)} {self.unit} "
+
+
+kitchen_ts = TempSensor("kitchen", "F")
+# kitchen_hs = Sensor("kitchen")
+print(kitchen_ts.read_temp())

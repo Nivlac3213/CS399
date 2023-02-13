92c69476a333ae8eefa86ff25faa7cc9531b68a3
Nivlac3213 <80916176+Nivlac3213@users.noreply.github.com>
2023-02-12 20:01:52 -0700

    OOP notes and Assignment, and main from class as an OOP example

---
+++ CS399/OOP/HenggelerCalvin_HW2_OOP.py
@@ -0,0 +1,193 @@
+"""
+Author:     Calvin Henggeler
+Date:       February 2, 2023
+Course:     CS399 Intermediate Python
+
+Description:
+            This python file is made to complete Homework 2 on the topic of
+            Object-Oriented Programming (OOP).
+Disclaimers:
+            None, No Artificial Intelligence tools used
+Sources:
+            No external Sources Used
+"""
+
+# =========================================================================== #
+#                                   Imports                                   #
+# =========================================================================== #
+from random import randint
+
+
+# =========================================================================== #
+#                                   Classes                                   #
+# =========================================================================== #
+class Marks:
+    """
+    Marks Super Class
+    IDK what this is, Grades probably?
+    """
+    # --- Attributes --- #
+    course = " "
+    letter_grade = " "
+    percent = 0.0
+
+    # ---  Methods  --- #
+    def __init__(self, course: str, percent: float):
+        self.course = course
+        self.percent = percent
+        self.letter_grade = (self.calculate_letter_grade(self.percent))
+
+    @property
+    def percent(self):
+        return self._percent
+
+    @percent.setter
+    def percent(self, value: float):
+        """
+        Ensures that grades are a real value > 0
+        :param value: grade percentage -> float
+        :return: None
+        """
+        if value < 0:
+            raise ValueError("Grade below 0% is not possible")
+        self._percent = value
+
+    @staticmethod
+    def calculate_letter_grade(grade_percentage):
+        """
+        gets the represented letter grade given a numerical value of the grade.
+        :param grade_percentage: float value representing numerical grade
+        :return: str of letter grade
+        """
+        if grade_percentage < 60:
+            return "F"
+        elif 60 <= grade_percentage < 70:
+            return "D"
+        elif 70 <= grade_percentage < 80:
+            return "C"
+        elif 80 <= grade_percentage < 90:
+            return "B"
+        elif 90 <= grade_percentage:
+            return "A"
+
+
+class Student:
+    """
+    Student Super Class
+    """
+    # --- Attributes --- #
+    student_name = " "      # string for name
+    student_id = 0          # id number
+    marks = []              # list of marks of the student
+
+    # ---  Methods  --- #
+    def __init__(self, name: str, student_id: int):
+        self.__class__.student_name = name
+        self.__class__.student_id = student_id
+        self.__class__.marks = []
+
+    def add_grade(self, new_mark: Marks):
+        """
+        adds a new mark/grade to the student profile (marks array attribute)
+        :param new_mark: Marks Object
+        :return:
+        """
+        self.__class__.marks.append(new_mark)
+
+    def print_grades(self):
+        """
+        Prints the student grades in a table
+        :return: None
+        """
+        print("Course  |  Letter  |  Percent")
+        for mark in self.marks:
+            print(f"{mark.course.ljust(6)}  |  {mark.letter_grade.ljust(6)}  |  {mark.percent:.2f}%")
+
+
+class Undergraduate(Student):
+    """
+    Undergraduate Student subclass
+    """
+    # --- Attributes --- #
+    sat_score = 0
+
+    # ---  Methods  --- #
+    def __init__(self, name: str, student_id: int, sat_score: int):
+        super().__init__(name=name, student_id=student_id)
+        self.sat_score = sat_score
+
+
+class Postgraduate(Student):
+    """
+    Postgraduate Student subclass
+    """
+    # --- Attributes --- #
+    bachelors_gpa = 0
+
+    # ---  Methods  --- #
+    def __init__(self, name: str, student_id: int, bachelors_gpa: float):
+        super().__init__(name=name, student_id=student_id)
+        self.bachelors_gpa = bachelors_gpa
+
+
+# =========================================================================== #
+#                              Execution/Test Code                            #
+# =========================================================================== #
+if __name__ == '__main__':
+    # Create new undergraduate student objects
+    StudentX = Undergraduate(name="Calvin Henggeler", student_id=1234567, sat_score=1500)
+    print(f"Student {StudentX.student_id}, whose name is {StudentX.student_name},"
+          f"scored a {StudentX.sat_score} on the SAT.\n")
+
+    # Add and print Grades
+    mark1 = Marks(course="CEC320", percent=91.2)   # These are not real
+    mark2 = Marks(course="EE401", percent=75.69)
+    mark3 = Marks(course="COM221", percent=40.04)
+    StudentX.add_grade(mark1)
+    StudentX.add_grade(mark2)
+    StudentX.add_grade(mark3)
+
+    StudentX.print_grades()
+
+    # Creating some other classes
+    StudentY = Postgraduate("Ed Post", 1010101, 3.95)
+    vars(StudentY)
+
+    mark1 = Marks(course="CEC420", percent=randint(1, 100))   # These are not real
+    mark2 = Marks(course="EE225", percent=randint(1, 100))
+    mark3 = Marks(course="CS399", percent=randint(1, 100))
+    StudentY.add_grade(mark1)
+    StudentY.add_grade(mark2)
+    StudentY.add_grade(mark3)
+
+    StudentZ = Student("Matthew Jaffe", 1111111)
+    vars(StudentZ)
+
+    mark1 = Marks(course="CEC320", percent=randint(1, 100))   # These are not real
+    mark2 = Marks(course="EE-401", percent=randint(1, 100))
+    mark3 = Marks(course="COM221", percent=randint(1, 100))
+    StudentZ.add_grade(mark1)
+    StudentZ.add_grade(mark2)
+    StudentZ.add_grade(mark3)
+
+    # testing instances
+    print("\nTesting if object are instances of a class")
+
+    print("StudentX")
+    print(isinstance(StudentX, Undergraduate))
+    print(isinstance(StudentX, Postgraduate))
+    print(isinstance(StudentX, Student))
+    print("StudentY")
+    print(isinstance(StudentY, Undergraduate))
+    print(isinstance(StudentY, Postgraduate))
+    print(isinstance(StudentY, Student))
+    print("StudentZ")
+    print(isinstance(StudentZ, Undergraduate))
+    print(isinstance(StudentZ, Postgraduate))
+    print(isinstance(StudentZ, Student))
+
+    student_roster = [StudentX, StudentY, StudentZ]
+
+    for student in student_roster:
+        print(f"\n{student.student_name} ID: {student.student_id}")
+        student.print_grades()
---
+++ CS399/OOP/OOP.py
@@ -0,0 +1,81 @@
+"""
+Module 1: Object Oriented Programming
+Date: 2/2/2023
+Author: Calvin Henggeler
+"""
+
+# ***** Class vs Objects *****
+# CLASS: collection of instance variables. A blueprint or template for objects.
+# OBJECTS: an Instance of a class
+
+# Python objects have constructors __new__ and inits __init__
+
+# Method/ operator overloading does not work in Python (ex .add() )
+# however a child class can override a method of a parent class
+
+from random import randint
+
+
+class Sensor:
+    """ Sensor Class for generic Sensor """
+    frequency = 10  # (Hz) can call the sensor every 10 time per second
+
+    def __init__(self, location: str):
+        """
+        Initialize the new ob ject of type Sensor
+        :param location: str -> where the sensor is located
+        :return: none
+        """
+        self.location = location
+        self.req_counter = 0
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def value(self) -> bool:
+        """
+        Models the frequency parameter for higher performing sensors respond
+        more often
+        :return: Bool
+        """
+        self.req_counter += 1
+        return self.req_counter % self.frequency == 0
+
+
+class TempSensor(Sensor):
+
+    def __init__(self, location: str, unit: str):
+        self.unit = unit
+        super().__init__(location)
+
+    def print_temp(self) -> None:
+        print(f"Humidity in the {self.location} is {randint(0,102)} {self.unit} ")
+        return None
+
+    def value(self) -> str:
+        if super().value():
+            return f"Temperature in the {self.location} is {randint(42, 105)} {self.unit}"
+
+
+class HumiditySensor(Sensor):
+    frequency = 20  # (Hz)
+
+    def value(self) -> str:
+        if super().value():
+            return f"Humidity in the {self.location} is {randint(10, 90)}"
+
+    def print_humidity(self) -> None:
+        print(f"Humidity in the {self.location} is {randint(10, 90)}")
+        return None
+
+
+if __name__ == "__main__":
+    kitchen_ts = TempSensor("kitchen", "F")
+    kitchen_hs = HumiditySensor("kitchen")
+
+    sl = [kitchen_ts, kitchen_hs]
+    for i in range(30):
+        for s in sl:
+            reading = s.value()
+            if reading:
+                print(f"{s.req_counter} .. {s} : {reading}")

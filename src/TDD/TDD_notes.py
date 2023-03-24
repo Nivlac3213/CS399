670061dc396cabb2a4531d101be20608518f3835
nivlac3213 <80916176+Nivlac3213@users.noreply.github.com>
2023-03-24 08:16:04 -0700

    TDD notes, Added test directory,

---
+++ src/TDD/TDD_notes.py
@@ -0,0 +1,47 @@
+"""
+Test Driven Development
+"""
+
+"""
+Iterative and repeatable process
+1) Consider a code module to be written
+2) Write the Test First to initially fail
+2) Write Minimal code to pass the test
+3) Run Code and test functionality
+
+New test per new feature
+
+Doctest v Pytest
+Fixtures
+Logging
+
+*** Pytest ****
+arrange: 
+Act: 
+
+*** Fixtures ***
+Decorated with @pytest.fixture
+This creates something repeatable
+fixtures are used if it will be used alot in development
+
+*** "Happy Path" *** 
+Happy path is expected values and cases,
+edge cases: occures in extreme operating parameters
+Corner Case: 
+
+*** Exceptions ***
+
+
+*** Logging ***
+Tracking evens that heppens when the program runs
+
+Debug: Detailed information for diagnosing problems (Too Much info here)
+Info: Confirmation that Program is working well
+Warning: Indication of something unexpected or indicative of a problem "disk space running low" software still runs normally
+Error: software has failed somewhere, not able to perform a function
+Critical: Serious Error, program crashed or is unable to continue
+
+
+
+
+"""
